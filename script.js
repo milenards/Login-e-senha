eeeddf257c14f5473d93445ef066be816480bd37
Millena <contatomillenarodrigues20@gmail.com>
2024-07-20 17:56:16 -0300

    teste de conexão com a tela de logins de RS

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function viewerSenha() {
 }
 
 
+
+function validarSenhas() {
+    var senha = document.getElementById("senha").value;
+    var confirmarSenha = document.getElementById("confirmar_senha").value;
+    if (senha != confirmarSenha) {
+        alert("As senhas precisam ser iguais.");
+        return false;
+    }
+    return true;
+}

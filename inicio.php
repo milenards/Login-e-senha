ce60eec0961d6c41ca116f1e51002b2ef2f89fbd
Millena <contatomillenarodrigues20@gmail.com>
2024-08-22 20:00:33 -0300

    tela inicial do sistema

---
+++ inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iniciar</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <a href="CadastrarEmp.php"> Cadastrar Empresa</a><br>
+    <a href="CadloginsEmp.php"> CadloginsEmp.php</a><br>
+    <a href="#"> Verificar empresas cadastradas</a>
+
+</body>
+</html>

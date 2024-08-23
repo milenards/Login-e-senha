f5de5c5bcd1752f6336eb43abd6c6de377bc3a83
Millena <contatomillenarodrigues20@gmail.com>
2024-08-22 22:18:17 -0300

    retirando o Underline

--- inicio.php
+++ inicio.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>iniciar</title>
+    <title>tela inicial</title>
     <link rel="stylesheet" href="estilo.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    <a href="CadastrarEmp.php"> Cadastrar Empresa</a><br>
-    <a href="CadloginsEmp.php"> CadloginsEmp.php</a><br>
-    <a href="#"> Verificar empresas cadastradas</a>
 
+<body>
+    <div class="background">
+        <div class="container text-center login-box">
+            <a class="link-offset-2 link-underline link-underline-opacity-0" href="CadastrarEmp.php" > Cadastrar Empresa</a><br>
+            <a class="link-offset-2 link-underline link-underline-opacity-0" href="CadloginsEmp.php"> CadloginsEmp.php</a><br>
+            <a class="link-offset-2 link-underline link-underline-opacity-0" href="#"> Verificar empresas cadastradas</a>
+        </div>
+    </div>
 </body>
+
 </html>

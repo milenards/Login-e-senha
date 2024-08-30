ba3e278fa6a51747872a89767021cf0cc514a4e9
Millena <contatomillenarodrigues20@gmail.com>
2024-08-29 21:04:34 -0300

    atualizando

--- cadastrarEmp.php
+++ cadastrarEmp.php
@@ -4,18 +4,21 @@ include 'conexaoEmp.php';
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de empresas</title>
     <link rel="stylesheet" href="estilo.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="background">
         <div class="container text-center login-box">
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" onsubmit="return validarSenhas();">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+                onsubmit="return validarSenhas();">
                 <div class="container center">
                     <h1>Cadastro de Empresas</h1>
                     <label for="empresa" class="form-label">Empresa:</label><br>
@@ -36,4 +39,5 @@ include 'conexaoEmp.php';
         </div>
     </div>
     </form>
+
 </html>

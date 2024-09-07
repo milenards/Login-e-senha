92e5b0f18c3da7e22c669ac641d4a1e97ffd0830
Millena <contatomillenarodrigues20@gmail.com>
2024-09-07 17:05:46 -0300

    adicionando icones

--- inicio.php
+++ inicio.php
@@ -12,9 +12,22 @@
 <body>
     <div class="background">
         <div class="container text-center login-box">
-            <a class="link-offset-2 link-underline link-underline-opacity-0" href="CadastrarEmp.php" > Cadastrar Empresa</a><br>
-            <a class="link-offset-2 link-underline link-underline-opacity-0" href="CadloginsEmp.php"> Cadastrar Logins das Empresas</a><br>
-            <a class="link-offset-2 link-underline link-underline-opacity-0" href="visualizarEmp.php"> Verificar empresas cadastradas</a>
+            <a class="link-dark link-underline link-underline-opacity-0" href="CadastrarEmp.php">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                    <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                    <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5" />
+                </svg>Cadastrar Empresa</a><br>
+
+
+            <!-- <a class="link-dark link-underline link-underline-opacity-0" href="CadloginsEmp.php"> Cadastrar Logins das Empresas</a><br> -->
+
+
+
+            <a class="link-dark link-underline link-underline-opacity-0" href="visualizarEmp.php">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" />
+                </svg>Verificar empresas cadastradas</a>
+                
         </div>
     </div>
 </body>
